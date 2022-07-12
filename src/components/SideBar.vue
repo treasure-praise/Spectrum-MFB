9499060b00f550f951472994a06d61e9d404b392
Treasure-Praise <mac@Treasure-Praises-MacBook-Pro.local>
2022-07-12 15:15:21 +0100

    made adjustments to sidebar

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -7,27 +7,49 @@
         <div class="flex flex-col justify-around  ">
             <div class="flex flex-col mx-7">
                 <div class="hover:bg-[#0098DA] w-full hover:rounded mx-auto px-5 py-3 text-left flex">
-                    <img src="../assets/icons/element-4.svg" alt="">
-                <h2 c>Dashboard</h2>
+                    <img src="../assets/icons/element-4light.svg" alt="">
+                <h2 class="px-5 text-base">Dashboard</h2>
                 </div>
                 
                 <div class="hover:bg-[#0098DA] w-full hover:rounded mx-auto px-5 py-3 text-left flex">
                     <img src="../assets/icons/note.svg" alt="">
-                <h2 >Transfer Request</h2> 
+                <h2 class="pl-5 text-base">Transfer Request</h2> 
                 </div>
                 
                 <div class="hover:bg-[#0098DA] w-full hover:rounded mx-auto px-5 py-3 text-left flex">
                     <img src="../assets/icons/users.svg" alt="">
-                    <h2 >User Management</h2>
+                    <h2 class="pl-5 ">User Management</h2>
+                </div>
+
+                 <div class="hover:bg-[#0098DA] w-full hover:rounded mx-auto px-5 text-left flex">
+                    <img src="../assets/icons/document-text.svg" alt="">
+                    <h2 class="hover:bg-[#0098DA] w-full hover:rounded mx-auto px-5 py-3 text-left">Approver</h2>
                 </div>
                 
-                <h2 class="hover:bg-[#0098DA] w-full hover:rounded mx-auto px-5 py-3 text-left">Report</h2>
-                <h2 class="hover:bg-[#0098DA] w-full hover:rounded mx-auto px-5 py-3 text-left">Messgae</h2>
-                <h2 class="hover:bg-[#0098DA] w-full hover:rounded mx-auto px-5 py-3 text-left">Settings</h2>
+                <div class="hover:bg-[#0098DA] w-full hover:rounded mx-auto px-5  text-left flex">
+                    <img src="../assets/icons/message.svg" alt="">
+                    <h2 class="hover:bg-[#0098DA] w-full hover:rounded mx-auto px-5 py-3 text-left">Messgae</h2>
+                </div>
+
+                <div class="hover:bg-[#0098DA] w-full hover:rounded mx-auto px-5  text-left flex">
+                    <img src="../assets/icons/setting-2.svg" alt="">
+                     <h2 class="hover:bg-[#0098DA] w-full hover:rounded mx-auto px-5 py-3 text-left">Settings</h2>
+                </div>            
+               
             </div>
             <div class="flex flex-col mx-7">
-                <h2 class="hover:bg-[#0098DA] w-full hover:rounded mx-auto px-5 py-3 text-left">Help</h2>
-                <h2 class="hover:bg-[#0098DA] w-full hover:rounded mx-auto px-5 py-3 text-left">Logout</h2>
+
+                <div class="hover:bg-[#0098DA] w-full hover:rounded mx-auto px-5  text-left flex">
+                    <img src="../assets/icons/Question.svg" alt="">
+                     <h2 class="hover:bg-[#0098DA] w-full hover:rounded mx-auto px-5 py-3 text-left">Help</h2>
+                </div>
+
+            <div class="hover:bg-[#0098DA] w-full hover:rounded mx-auto px-5 text-left flex">
+                    <img src="../assets/icons/SignOut.svg" alt="">
+                     <h2 class="hover:bg-[#0098DA] w-full hover:rounded mx-auto px-5 py-3 text-left">Logout</h2>
+                </div>
+                
+                
             </div>
         </div>
     </div>
